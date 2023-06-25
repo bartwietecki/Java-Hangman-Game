f98e5ac92e46afdc33a8e6383a1bd583a898786f
Barto <bartwietecki@gmail.com>
2023-06-25 11:59:12 +0200

    Hangman image label and category label

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -3,6 +3,12 @@ import java.awt.*;
 public class CommonConstants {
     // file path
     public static final String DATA_PATH = "resources/data.txt";
+    public static final String IMAGE_PATH = "resources/1.png";
+
+    // color
+    public static final Color PRIMARY_COLOR = Color.decode("#14212D");
+    public static final Color SECONDARY_COLOR = Color.decode("#FCA311");
+    public static final Color BACKGROUND_COLOR = Color.decode("#101820");
 
     // size config
     public static final Dimension FRAME_SIZE = new Dimension(540, 760);
--- src/Hangman.java
+++ src/Hangman.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Hangman extends JFrame {
+    // counts the number of incorrect guesses player has made
+    private int incorrectGuesses;
+
+    // store the challenge from the WordDB here
+    private String[] wordChallenge;
+
+    private final WordDB wordDB;
+    private JLabel hangmanImage, categoryLabel;
+
+
     public Hangman() {
         super("Hangman Game (Java Ed.)");
         setSize(CommonConstants.FRAME_SIZE);
@@ -8,5 +19,35 @@ public class Hangman extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
+
+        // init vars
+        wordDB = new WordDB();
+        wordChallenge = wordDB.loadChallenge();
+
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents() {
+        // hangman image
+        hangmanImage = CustomTools.loadImage(CommonConstants.IMAGE_PATH);
+        hangmanImage.setBounds(0, 0, hangmanImage.getPreferredSize().width, hangmanImage.getPreferredSize().height);
+
+        // category display
+        categoryLabel = new JLabel(wordChallenge[0]);
+        categoryLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        categoryLabel.setOpaque(true);
+        categoryLabel.setForeground(Color.WHITE);
+        categoryLabel.setBackground(CommonConstants.SECONDARY_COLOR);
+        categoryLabel.setBorder(BorderFactory.createLineBorder(CommonConstants.SECONDARY_COLOR));
+        categoryLabel.setBounds(
+                0,
+                hangmanImage.getPreferredSize().height - 28,
+                CommonConstants.FRAME_SIZE.width,
+                categoryLabel.getPreferredSize().height
+        );
+
+        getContentPane().add(categoryLabel);
+        getContentPane().add(hangmanImage);
     }
 }
